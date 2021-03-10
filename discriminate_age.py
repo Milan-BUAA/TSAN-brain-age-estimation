2829d9eff693945a0806b3fb2afb6d68a27dce3c
liuziyang1106 <liuziyang1106@sina.com>
2021-03-10 18:11:08 +0800

    comment discriminate age

--- discriminate_age.py
+++ discriminate_age.py
@@ -2,6 +2,16 @@ import torch
 import numpy as np 
 
 def discriminate_age(age, range=5):
+    '''
+    [summary]
+
+    Args:
+        age (numpy array): [predicted brain age from first stage network, which is needed to discriminate.]
+        range (int, optional): [discritized delta]. Defaults to 5.
+
+    Returns:
+        [numpy array]: [discritized predicted brain age]
+    '''
     dis = []
     for i in age:
         value = i // range
