97046144a6aa4dec5662d536e7768dc135fcc8c9
liuziyang1106 <liuziyang1106@sina.com>
2021-06-01 12:57:13 +0800

    fix(bash test): add hyperparameter in bash test file

--- bash_test_first_stage.sh
+++ bash_test_first_stage.sh
@@ -9,14 +9,13 @@ sorter_path=./Sodeep_pretrain_weight/best_lstmla_slen_${batch_size}.pth.tar
 
 # ------ train and set the parameter
 CUDA_VISIBLE_DEVICES=0 python prediction_first_stage.py \
---model             ${model}                            \
---batch_size        $batch_size                                   \
---num_workers       20                                  \
---output_dir        ${model_dirpath}                    \
---model_name        ${model}_best_model.pth.tar         \
---test_folder       ${test_dirpath}                     \
---excel_path        ${excel_dirpath}                    \
---npz_name          brain_age.npz                       \
---sorter            ${sorter_path}                      \
+--model             ${model}                            \ # Deep learning model to do brain age estimation
+--batch_size        $batch_size                         \ # Batch size during training process
+--output_dir        ${model_dirpath}                    \ # Output dictionary, whici will contains training log and model checkpoint
+--model_name        ${model}_best_model.pth.tar         \ # Checkpoint file name
+--test_folder       ${test_dirpath}                     \ # Test set data path
+--excel_path        ${excel_dirpath}                    \ # Excel file path
+--npz_name          brain_age.npz                       \ # npz file name to store predited brain age
+--sorter            ${sorter_path}                      \ # When use ranking loss, the pretrained SoDeep sorter network weight need to be appointed
 
 
--- bash_test_second_stage.sh
+++ bash_test_second_stage.sh
@@ -9,17 +9,16 @@ first_stage_net=./pretrained_model/ScaleDense/ScaleDense_best_model.pth.tar
 
 # ------ train and set the parameter
 CUDA_VISIBLE_DEVICES=0 python prediction_second_stage.py \
---model             ${model}                             \
---batch_size        $batch_size                          \
---num_workers       20                                   \
---output_dir        ${model_dirpath}                     \
---model_name        ${model}_best_model.pth.tar          \
---test_folder       ${test_dirpath}                      \
---excel_path        ${excel_dirpath}                     \
---npz_name          brain_age.npz                        \
---dis_range         5                                    \
---first_stage_net   ${first_stage_net}                   \
---sorter            ${sorter_path}                       \
+--model             ${model}                             \ # Deep learning model to do brain age estimation
+--batch_size        $batch_size                          \ # Batch size during training process
+--output_dir        ${model_dirpath}                     \ # Output dictionary, whici will contains training log and model checkpoint
+--model_name        ${model}_best_model.pth.tar          \ # Checkpoint file name
+--test_folder       ${test_dirpath}                      \ # Test set data path
+--excel_path        ${excel_dirpath}                     \ # Excel file path
+--npz_name          brain_age.npz                        \ # npz file name to store predited brain age
+--dis_range         5                                    \ # Discritize step when training the second stage network
+--first_stage_net   ${first_stage_net}                   \ # trained first stage network checkpoint file path
+--sorter            ${sorter_path}                       \ # When use ranking loss, the pretrained SoDeep sorter network weight need to be appointed
 
 
 
